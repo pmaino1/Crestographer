667dd40aafb12d2d0be17e24f488e1eece5e653b
Paul <pcmaino@gmail.com>
2019-10-18 12:18:20 -0400

    Added formatted json library.

--- app/src/main/java/com/example/tableproject/MainActivity.java
+++ app/src/main/java/com/example/tableproject/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.tableproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+
+        InputStream is = getResources().openRawResource(R.raw.fesheet);    }
 }

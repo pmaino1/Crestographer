e59dbb72a47dfc3276409deaa63fbe3308909c45
Paul <pcmaino@gmail.com>
2019-10-23 12:17:47 -0400

    Implemented Google Sheets read-only functionality.

---
+++ app/src/main/java/com/example/tableproject/Config.java
@@ -0,0 +1,6 @@
+package com.example.tableproject;
+
+public class Config {
+    public static String google_api_key = "AIzaSyDAKGB_xhETR5eJpyVX07_XuSO1hvMX_xE";
+    public static String spreadsheet_id = "1jNV4aHMHj4AjCj3ued51Rr4nHZYEyfkGOwk1KuW3huE";
+}
--- app/src/main/java/com/example/tableproject/MainActivity.java
+++ app/src/main/java/com/example/tableproject/MainActivity.java
@@ -4,22 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import java.util.List;
 
-import org.json.JSONObject;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Value;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
     //try RecyclerView!!
 
-    AssetManager am;
-    InputStream is;
-    JSONObject fejson;
     TextView testText;
 
     @Override
@@ -29,27 +33,35 @@ public class MainActivity extends AppCompatActivity {
 
         testText = findViewById(R.id.testText);
 
-        try {
-            am = getAssets();
-            is = am.open("fesheet.json");
-
-            BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            StringBuilder responseStrBuilder = new StringBuilder();
-            System.out.println("its goin");
-
-            String inputStr;
-            while ((inputStr = streamReader.readLine()) != null)
-                responseStrBuilder.append(inputStr);
-            fejson = new JSONObject(responseStrBuilder.toString());
-
-            testText.setText(fejson.getString("Name"));
-        }
-        catch(Exception e) {
-            System.out.println("Json parsing ERROR!");
-            System.exit(-1);
-        }
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory factory = JacksonFactory.getDefaultInstance();
+        final Sheets sheetsService = new Sheets.Builder(transport, factory, null)
+                .setApplicationName("TableProject")
+                .build();
+        final String spreadsheetId = Config.spreadsheet_id;
 
 
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String range = "Characters!A1:B2";
+                    ValueRange result = sheetsService.spreadsheets().values()
+                            .get(spreadsheetId, range)
+                            .setKey(Config.google_api_key)
+                            .execute();
+                    int numRows = result.getValues() != null ? result.getValues().size() : 0;
+                    System.out.println("SUCCESS.\n"+ "rows retrived " + numRows);
 
+
+                    for(List row:result.getValues()) {
+                        System.out.println(row.get(0));
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println("Sheets failed" + e.getLocalizedMessage());
+                }
+            }
+        }.start();
     }
 }

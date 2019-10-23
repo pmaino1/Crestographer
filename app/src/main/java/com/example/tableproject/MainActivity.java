190e393617607bcbc2979c1c0ed2cdbd34ccdcb7
Paul <pcmaino@gmail.com>
2019-10-22 21:12:13 -0400

    Having trouble with current approach. Going to attempt using RecyclerView and the Google Drive API next session.

--- app/src/main/java/com/example/tableproject/MainActivity.java
+++ app/src/main/java/com/example/tableproject/MainActivity.java
@@ -4,28 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //try RecyclerView!!
+
     AssetManager am;
     InputStream is;
+    JSONObject fejson;
+    TextView testText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        testText = findViewById(R.id.testText);
+
         try {
             am = getAssets();
             is = am.open("fesheet.json");
+
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            StringBuilder responseStrBuilder = new StringBuilder();
+            System.out.println("its goin");
+
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null)
+                responseStrBuilder.append(inputStr);
+            fejson = new JSONObject(responseStrBuilder.toString());
+
+            testText.setText(fejson.getString("Name"));
         }
         catch(Exception e) {
-            System.out.println("IO ERROR!");
+            System.out.println("Json parsing ERROR!");
             System.exit(-1);
         }
+
+
 
     }
 }

9a7ee8fa976a905ba444e8c4c6d4cda857690140
Paul Maino <pcmaino@gmail.com>
2019-11-30 16:51:58 -0500

    Delete Config.java

--- app/src/main/java/com/example/tableproject/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.tableproject;
-
-public class Config {
-    public static String google_api_key = "AIzaSyDAKGB_xhETR5eJpyVX07_XuSO1hvMX_xE";
-    public static String spreadsheet_id = "1jNV4aHMHj4AjCj3ued51Rr4nHZYEyfkGOwk1KuW3huE";
-}
